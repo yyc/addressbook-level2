898c36f4bb54889e596918fc54879ae7ab78a709
Yuchuan <yyc478@gmail.com>
2016-09-09 01:53:03 +0800

    Make Address, Email, Name and Phone implement Printable

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -1,15 +1,15 @@
 package seedu.addressbook.data.person;
 
-import seedu.addressbook.data.exception.IllegalValueException;
-
 import java.util.Arrays;
 import java.util.List;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
  * Represents a Person's name in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
-public class Name {
+public class Name implements Printable {
 
     public static final String EXAMPLE = "John Doe";
     public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphabetic characters";
@@ -61,4 +61,8 @@ public class Name {
         return fullName.hashCode();
     }
 
+	@Override
+	public String getPrintableString() {
+		return fullName;
+	}
 }
